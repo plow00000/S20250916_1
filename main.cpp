64151dca76ed492859d6c0387e9281a74cea3e18
plow00000 <icusene@gmail.com>
2025-09-16 10:42:32 +0900

    기초 게임 정보 추가
    추가된 요소
    - 플레이어 체력, 방어력, 저항력
    - 몬스터 공격력, 주문력
    - 데미지 및 플레이어 체력 계산

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
+	//연산자 우선순위 : *, /, % > +, -
 
+	//플레이어 정보
+	int PlayerHP = 100;
+	int PlayerDef = 20;
+	int PlayerReg = 3;
+
+	//몬스터 정보
+	int MonsterAD = 30;
+	int MonsterAP = 10;
+
+	int Damage = (MonsterAD - PlayerDef) + (MonsterAP - PlayerReg);
+
+	PlayerHP = PlayerHP - Damage;
+
+	cout << "플레이어가 몬스터에게 입은 피해량 : " << Damage << endl << "현재 플레이어 HP : " << PlayerHP << endl;
 
 	return 0;
 }
